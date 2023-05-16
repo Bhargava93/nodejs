f0a652e774861c5e9346b85a2f079a9249549b70
bhargava <bhargavahara@gmail.com>
2023-05-16 20:52:12 +0530

    get, post

--- index.js
+++ index.js
@@ -1,38 +1,72 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
 
-app.use(express.json());
 
-// app.use(bhargava())
-
-// Basic crud operations
-
-// crud
-// c - create
-// r - read
-// u - update
-// d - delete
+// curd oprations
 
 // desc: create user
 // method: post
-// url: /createUser
+// path: http://localhost:3000/createUser
 
 app.post("/createUser", (req, res) => {
 
-    console.log("function invoked: createuser");
-    console.log("req===",req.body);
-
-    res.send({ status: true, message: "user created", data:req.body });
+    console.log("req object====", req.body);
+    res.send({ message: "user has created successfully", data: req.body });
 
 });
 
-app.get("/family",(req,res)=>{
-   
+// desc: fetch user deatils
+// method: get
+// path: http://localhost:3000/fetchUsers
 
-});
+app.get("/fetchUsers/:id", (req, res) => {
+
+    console.log("query====", req.params);
+    let array = [{
+        "id": "1",
+        "firstname": "bhargava",
+        "surname": "kondreddy",
+        "mobile": 7569610097,
+        "gender": "male"
+    },
+    {
+        "id": "2",
+        "firstname": "Pranay",
+        "surname": "bandlapalli",
+        "mobile": 9876543210,
+        "gender": "male"
+    },
+    {
+        "id": "3",
+        "firstname": "dhana",
+        "surname": "uppithi",
+        "mobile": 7093878067,
+        "gender": "male"
+    }
+    ]
+
+    // let result=array.filter(ele =>{
+    //     // console.log(ele);
+    //     if(ele["id"] === req.params.id){
+    //         return ele;
+    //     }
+    // })
+
+    let result = array.filter(ele => ele["id"] === req.params.id);
+    console.log("result===", result);
+
+    res.send({
+        "message": "user details",
+        "data": result
+    });
+})
+
 
 app.listen(3000, () => {
-    console.log("server running on 3000 port ");
-    console.log("url: http://localhost:3000");
+    console.log("server running on 3000 port");
 });
+
+// http://localhost:3000
+// http://127.0.0.0:3000
